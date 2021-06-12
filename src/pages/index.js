41b06ffcda2a74305e7389e7b1ce882acc688dbd
Ubaid Ali <syedubaidali2020@gmail.com>
2021-06-12 11:06:13 +0500

    create serverless function bookmark

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
 import React from "react"
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+
+// This query is executed at run time by Apollo.
+const APOLLO_QUERY = gql`
+{
+  message
+}
+`;
 
 export default function Home() {
-  return <div>This is Index.js file!</div>
+  const { loading, error, data } = useQuery(APOLLO_QUERY);
+  console.log(`data`, data)
+
+  return (
+      <div>
+        <h2>Data Received from Apollo Client at runtime from Serverless Function:</h2>
+        {loading && <p>Loading Client Side Querry...</p>}
+        {error && <p>Error: ${error.message}</p>}
+        {data && data.message && (
+          <div>{data.message}</div>
+        )}
+      </div>
+  );
+    
 }
