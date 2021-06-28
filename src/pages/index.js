06cc60548aa827940bcbeac96fc5262c5383442f
Ubaid Ali <syedubaidali2020@gmail.com>
2021-06-28 22:33:38 +0500

    add styling
    add material-ui styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,53 @@ import React from "react"
 import { useQuery, useMutation } from "@apollo/client"
 import gql from "graphql-tag"
 
+// material-ui
+import { makeStyles } from "@material-ui/core/styles"
+import Paper from "@material-ui/core/Paper"
+import Grid from "@material-ui/core/Grid"
+import TextareaAutosize from "@material-ui/core/TextareaAutosize"
+import Button from "@material-ui/core/Button"
+import Accordion from "@material-ui/core/Accordion"
+import AccordionSummary from "@material-ui/core/AccordionSummary"
+import AccordionDetails from "@material-ui/core/AccordionDetails"
+import Typography from "@material-ui/core/Typography"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+
+// styles
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    fontWeight: 500,
+    backgroundColor: "#00e676",
+    padding: "2%",
+  },
+  grid: {
+    marginTop: "10px",
+    boxShadow: "0 0 4px #1b5e20",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    backgroundColor: "",
+  },
+  inputLink: {
+    width: "80%",
+  },
+  inputDesc: {
+    width: "80%",
+  },
+  button: {
+    margin: theme.spacing(1),
+    backgroundColor: "#00e676",
+    fontWeight: "500",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}))
+
 // This query is executed at run time by Apollo.
 const APOLLO_QUERY = gql`
   {
@@ -15,14 +62,16 @@ const APOLLO_QUERY = gql`
 
 const addBookmarkMutation = gql`
   mutation addBookmark($url: String!, $desc: String!) {
-    addBookmark(url:$url, desc:$desc) {
+    addBookmark(url: $url, desc: $desc) {
       url
     }
   }
 `
 
 export default function Home() {
-  const { loading, error, data} = useQuery(APOLLO_QUERY)
+  const classes = useStyles()
+
+  const { loading, error, data } = useQuery(APOLLO_QUERY)
   const [addBookmark] = useMutation(addBookmarkMutation)
   console.log(`data`, data)
 
@@ -35,36 +84,80 @@ export default function Home() {
         url: textfield.value,
         desc: desc.value,
       },
+      refetchQueries: [{ query: APOLLO_QUERY }],
     })
-    console.log(`textfield`, textfield.value)
-    console.log(`desc`, desc.value)
   }
 
   return (
-    <div>
-      <form onSubmit={addBookmarkSubmit}>
-        <input type="text" ref={node => (textfield = node)} />
-        <input type="text" ref={node => (desc = node)} />
-        <button type="submit">Add Bookmark</button>
-      </form>
-      <h2>
-        Data Received from Apollo Client at runtime from Serverless Function:
-      </h2>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: ${error.message}</p>}
-      {data && data.bookmark && (
-        <div>
-          <ul>
-            {data.bookmark.map(obj => {
-              return (
-                <li key={obj.id}>
-                  {obj.url} <br /> {obj.desc}
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      )}
+    <div className={classes.root}>
+      <Grid item xs={12} className={classes.grid}>
+        <Paper className={classes.paper}>
+          <h2>Bookmark Application</h2>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} className={classes.grid}>
+        <form onSubmit={addBookmarkSubmit}>
+          <Paper className={classes.paper}>
+            <TextareaAutosize
+              className={classes.inputLink}
+              ref={node => (textfield = node)}
+              aria-label="minimum height"
+              rowsMin={2}
+              placeholder="Enter Link"
+              required
+            />
+            <br />
+            <TextareaAutosize
+              className={classes.inputDesc}
+              ref={node => (desc = node)}
+              aria-label="minimum height"
+              rowsMin={2}
+              placeholder="Enter Description"
+              required
+            />
+            <br />
+            <Button
+              type="submit"
+              variant="contained"
+              color="default"
+              className={classes.button}
+            >
+              Add Bookmark
+            </Button>
+          </Paper>
+        </form>
+      </Grid>
+      <Grid item xs={12} className={classes.grid}>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: ${error.message}</p>}
+        {data && data.bookmark && (
+          <Paper className={classes.paper}>
+            <ul>
+              {data.bookmark.map(b => {
+                return (
+                  <Accordion key={b.id}>
+                    {/* bookmark link */}
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      <Typography className={classes.heading}>
+                        {b.url}
+                      </Typography>
+                    </AccordionSummary>
+
+                    {/* bookmark description */}
+                    <AccordionDetails>
+                      <Typography>{b.desc}</Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                )
+              })}
+            </ul>
+          </Paper>
+        )}
+      </Grid>
     </div>
   )
 }
